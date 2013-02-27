c9f7afdb067e7c93366df002307635e9eb72a4ce
HeajeongKim <heajeong0529@gmail.com>
2013-02-27 00:50:20 +0000

    REMOVE: select options for the game

--- CreateUpperPanel.java
+++ CreateUpperPanel.java
@@ -1,48 +1,39 @@
 package MineSweeper;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
-public class CreateUpperPanel extends JPanel implements ActionListener {
-
-	int index = 0;
-	int bombNumber = 10;
-	int gridColumn = 9;
-	int gridRow = 9;
+public class CreateUpperPanel extends JPanel{
 
 	public CreateUpperPanel() {
-		String[] options = { "10 bombs(9*9)", "40 bombs(16*16)",
-				"99 bombs(16*30)" };
-
-		JComboBox list = new JComboBox(options);
-		list.setSelectedIndex(0);
-		list.addActionListener(this);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JComboBox cb = (JComboBox) e.getSource();
-		int newSelection = cb.getSelectedIndex();
-
-		switch (newSelection) {
-		case 0:
-			bombNumber = 10;
-			gridColumn = 9;
-			gridRow = 9;
-			break;
-		case 1:
-			bombNumber = 40;
-			gridColumn = 16;
-			gridRow = 16;
-			break;
-		case 2:
-			bombNumber = 99;
-			gridColumn = 16;
-			gridRow = 30;
-			break;
-		}
+		this.setLayout(new GridLayout(3,1));
 		
 	}
 
+
+	private JComponent createSmileButton(){
+		BufferedImage buttonIcon = null;
+		try {
+			buttonIcon = ImageIO.read(new File("smile.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JButton button = new JButton(new ImageIcon(buttonIcon));
+		return button;
+	}
+	
 }
